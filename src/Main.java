f1974baef32294a386d669a74db1592f2f4f996e
Ruslan Fursa <fursa_1986@mail.ru>
2023-09-11 12:05:20 +0300

    add some map method examples

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -10,6 +10,21 @@ public class Main {
         list.add("Dear");
         list.add("Friends");
         List<String> list2 = list.stream().map(s -> s + " " + "Stream").toList();
-        System.out.println(list2);
+//        System.out.println(list2);
+
+        int[] array = {2,5,7,11,45};
+        array = Arrays.stream(array).map(element -> element + 10).toArray();
+        System.out.println(Arrays.toString(array));
+
+        Set<String> set = new TreeSet<>();
+        set.add("Hello");
+        set.add("My");
+        set.add("Dear");
+        set.add("Friends");
+
+        System.out.println(set);
+
+        Set<Integer> set2 = set.stream().map(element->element.length()).collect(Collectors.toSet());
+        System.out.println(set2);
     }
 }
